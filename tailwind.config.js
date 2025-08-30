e69cf118bd957bfa0cdb43a77c65c1e8c19204a3
Jonathan Braaten Mjøs <jonathan@mjos.no>
2025-08-30 13:50:07 +0200

    remove heroui from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 // tailwind.config.js
-const {heroui} = require("@heroui/theme");
+
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/@heroui/theme/dist/components/(button|ripple|spinner).js",
+  
   ],
   theme: {
     extend: {},
