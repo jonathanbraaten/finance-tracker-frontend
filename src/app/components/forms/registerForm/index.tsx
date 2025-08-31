47e46e6be43b0fba1133e33edd234b0a9dadc630
Jonathan Braaten Mjøs <jonathan@mjos.no>
2025-08-31 20:31:54 +0200

    feat(): user can register new account

--- src/app/components/forms/registerForm/index.tsx
+++ src/app/components/forms/registerForm/index.tsx
@@ -1,52 +1,71 @@
 'use client';
-import {
-  useForm,
-  SubmitHandler,
-  FieldErrors,
-  UseFormRegister,
-} from 'react-hook-form';
+/**
+ * Hook form imports.
+ */
+import { useForm, FieldErrors, UseFormRegister } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { registerSchema, RegisterSchema } from 'app/components/forms/schemas';
-import { useEffect } from 'react';
 
+/**
+ * Zod schema imports.
+ */
+import { registerSchema, RegisterSchema } from 'app/components/forms/schemas';
+/**
+ * Schema type.
+ */
 type InputFields = keyof RegisterSchema;
+
+/**
+ * Base url.
+ */
+const url = process.env.NEXT_PUBLIC_API_URL ?? '';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
 export default function RegisterForm() {
+  const router = useRouter();
+  const [error, setError] = useState('');
+
+  /**
+   * Hook form.
+   */
   const {
     register,
     handleSubmit,
-
     formState: { errors },
   } = useForm({ resolver: zodResolver(registerSchema) });
-  function onSubmit(data: RegisterSchema) {
-    console.log(data);
-    console.log(errors);
-  }
 
-  useEffect(() => console.log(errors), [errors]);
-  const createInput = (
-    reg: UseFormRegister<RegisterSchema>,
-    name: InputFields,
-    label: string,
-    errs: FieldErrors<RegisterSchema>,
-  ) => {
-    const errMsg = errs[name]?.message as string | undefined;
-    return (
-      <div className="flex flex-col gap-1">
-        <label className="sr-only absolute inset-o" htmlFor={name}>
-          {label}
-        </label>
-        <input
-          className="border border-gray-200 rounded-md p-1 placeholder:px-1"
-          placeholder={label}
-          id={name}
-          {...reg(name)}
-        />
-        <label className="min-h-[2vh] px-1" htmlFor={`error-${errMsg}`}>
-          {errMsg && <p className="text-red-600 text-xs">{errMsg}</p>}
-        </label>
-      </div>
-    );
-  };
+  /**
+   * Submit handler.
+   */
+  async function onSubmit(data: RegisterSchema) {
+    if (!data && errors) {
+      return;
+    }
+
+    try {
+      const response = await fetch(`${url}/accounts/signup`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        const error = await response.json();
+        throw new Error(error.message);
+      }
+      setError('');
+      router.replace('/login');
+    } catch (error: unknown) {
+      if (typeof error === 'object' && error !== null && 'message' in error) {
+        setError(error.message as string);
+      } else {
+        console.log('An unknown error occurred in catch');
+      }
+    }
+  }
 
   return (
     <form
@@ -64,6 +83,38 @@ export default function RegisterForm() {
       >
         submit
       </button>
+      <div className="text-xs min-h-[2vh]">
+        <p className="text-red-600">{error && error}</p>
+      </div>
     </form>
+  );
+}
+
+/**
+ * Create input fields.
+ */
+function createInput(
+  reg: UseFormRegister<RegisterSchema>,
+  name: InputFields,
+  label: string,
+  errs: FieldErrors<RegisterSchema>,
+) {
+  const errMsg = errs[name]?.message as string | undefined;
+  return (
+    <div className="flex flex-col gap-1">
+      <label className="sr-only absolute inset-o" htmlFor={name}>
+        {label}
+      </label>
+      <input
+        type={name === 'password' ? 'password' : 'text'}
+        className="border border-gray-200 rounded-md p-1 placeholder:px-1"
+        placeholder={label}
+        id={name}
+        {...reg(name)}
+      />
+      <label className="min-h-[2vh] px-1" htmlFor={`error-${errMsg}`}>
+        {errMsg && <p className="text-red-600 text-xs">{errMsg}</p>}
+      </label>
+    </div>
   );
 }
--- src/app/components/forms/schemas.ts
+++ src/app/components/forms/schemas.ts
@@ -1,17 +1,21 @@
 import * as z from 'zod';
 
 export const registerSchema = z.object({
-  first_name: z.string().min(1, { message: 'First name is required.' }),
-  last_name: z.string().min(1, { message: 'Last name is required.' }),
-  email: z.email({ message: 'Invalid email address.' }),
-  password: z.string().min(1, { message: 'Password is required.' }),
+  first_name: z.string().min(1, { error: 'First name is required.' }).trim(),
+  last_name: z.string().min(1, { error: 'Last name is required.' }).trim(),
+  email: z
+    .email({ error: 'Invalid email address.' })
+    .min(1, { error: 'Email is required' })
+
+    .trim(),
+  password: z.string().min(1, { error: 'Password is required.' }).trim(),
 });
 
 export type RegisterSchema = z.infer<typeof registerSchema>;
 
 export const loginSchema = z.object({
-  email: z.email({ message: 'Invalid email address.' }),
-  password: z.string().min(1, { message: 'Password is required.' }),
+  email: z.email({ error: 'Invalid email address.' }).trim(),
+  password: z.string().min(1, { error: 'Password is required.' }).trim(),
 });
 
 export type LoginSchema = z.infer<typeof loginSchema>;
