179be89824767794088821a883d03f32fe53d095
Jonathan Braaten Mjøs <jonathan@mjos.no>
2025-09-02 09:25:32 +0200

    feat():hero ui

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,18 +1,20 @@
 import Header from 'app/components/dashboard/header';
 import Sidebar from 'app/components/dashboard/sidebar';
-
+import { Providers } from 'app/providers';
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <section className="h-screen flex">
-      <Sidebar />
-      <div className="flex flex-col  grow">
-        <Header />
-        <section className="grow px-2 bg-gray-100/20">{children}</section>
-      </div>
-    </section>
+    <Providers>
+      <section className="h-screen flex">
+        <Sidebar />
+        <div className="flex flex-col  grow">
+          <Header />
+          <section className="grow px-2 bg-gray-100/20">{children}</section>
+        </div>
+      </section>
+    </Providers>
   );
 }
